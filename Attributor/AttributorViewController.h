5936f0ebed34df1c1987600bbdf554fc38a66fdf
Patrick Dawson <patrick.soboljew@gmail.com>
2014-07-06 13:47:51 +0200

    Initial Commit

---
+++ Attributor/AttributorViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributorViewController.h
+//  Attributor
+//
+//  Created by Patrick Dawson on 06.07.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorViewController : UIViewController
+
+@end
---
+++ Attributor/AttributorViewController.m
@@ -0,0 +1,29 @@
+//
+//  AttributorViewController.m
+//  Attributor
+//
+//  Created by Patrick Dawson on 06.07.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AttributorViewController.h"
+
+@interface AttributorViewController ()
+
+@end
+
+@implementation AttributorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
